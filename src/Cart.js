46550c80e59a15618a4544cfa6eee120aefe2961
Vivek <vivekraj78965.vr@gmail.com>
2022-09-16 07:43:39 +0530

    addding props to cart items

--- src/Cart.js
+++ src/Cart.js
@@ -2,16 +2,51 @@ import React from "react";
 import Cartitem from './Cartitem';
 
 class Cart extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            products: [
+                {
+                    id:1,
+                    price: 99,
+                    title: 'Watch',
+                    qty: 1,
+                    img: ''
+                },
+                {
+                    id:2,
+                    price: 999,
+                    title: 'Phone',
+                    qty: 10,
+                    img: ''
+                },
+                {
+                    id:3,
+                    price: 999,
+                    title: 'Laptop',
+                    qty: 4,
+                    img: ''
+                }
+            ]
+        }
+
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
     render() {
+        const { products } = this.state;
         return (
             <div className="cart">
-
-                < Cartitem />
-                < Cartitem />
-                < Cartitem />
+                {products.map((product)=> {
+                    return <Cartitem 
+                    product={product} 
+                    key={product.id} 
+                    />
+                })}
 
             </div>
-        )
+        );
+
     }
 }
 
--- src/Cartitem.js
+++ src/Cartitem.js
@@ -2,17 +2,7 @@ import React from "react";
 
 
 class CartItem extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            price: 999,
-            title: 'Phone',
-            qty: 1,
-            img: ''
-        }
-
-        // this.increaseQuantity = this.increaseQuantity.bind(this);
-    }
+   
     increaseQuantity = () => {
         //   there are basically two ways to increase quantity
         // setState form1 - if previous is not required use this
@@ -44,7 +34,7 @@ class CartItem extends React.Component {
     }
 
     render() {
-        const { price, title, qty } = this.state;
+        const { price, title, qty } = this.props.product;
         return (
             <div className="cart-item">
                 <div className="left-block">
